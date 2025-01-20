75afbc3cb64c98a9f20bcc25072282aca6e7d054
Kieran <canter.kieran@gmail.com>
2025-01-19 21:51:48 -0500

    Tied extract, transform, and load together to establish a functioning ETL pipeline

--- data/etl/main.py
+++ data/etl/main.py
@@ -1,1 +1,51 @@
+###
 # Run ETL pipeline
+###
+
+# Extract imports
+from extract import fetch_replays
+from datetime import datetime, timedelta
+
+# Transform imports
+from transform import compress_replays_for_s3
+
+# Load imports
+from load import load_to_postgres, load_to_s3
+
+def pipeline():
+    ###
+    # Step 1: Extract Data from Ballchasing API
+    ###
+    playlists = [ "unranked-duels", "unranked-doubles", "unranked-standard", "unranked-chaos",
+            "ranked-duels", "ranked-doubles", "ranked-standard"
+        ]
+
+    ranks = ["unranked", "bronze-1", "bronze-2", "bronze-3", "silver-1", "silver-2", "silver-3", "gold-1", "gold-2", "gold-3", "platinum-1", "platinum-2", "platinum-3", "diamond-1", "diamond-2", "diamond-3", "champion-1", "champion-2", "champion-3", "grand-champion-1", "grand-champion-2", "grand-champion-3", "supersonic-legend"]
+
+    # For initial ingestion, set time_period_days to 90
+    # Otherwise, set to appropriate number of days back according to regularity of data ingestion
+    #   e.g. if running weekly, set to 7, if running daily, set to 1
+    replay_date = datetime.today()
+    time_period_days = 1
+
+    total_replays = 0
+    for days_back in range(time_period_days):
+        replay_date = datetime.today() - timedelta(days=days_back)
+        for playlist in playlists:
+            if playlist.startswith("unranked"):
+                replays = fetch_replays(replay_date, playlist, "unranked")
+                total_replays += len(replays)
+                compressed_replays = compress_replays_for_s3(replays)
+                load_to_s3(compressed_replays, replay_date, playlist, "unranked")
+            else:
+                for rank in ranks:
+                    replays = fetch_replays(replay_date, playlist, rank)
+                    total_replays += len(replays)
+                    compressed_replays = compress_replays_for_s3(replays)
+                    load_to_s3(compressed_replays, replay_date, playlist, rank)
+    
+    print(f"Finished fetching: fetched {total_replays} replays.")
+
+
+if __name__ == "__main__":
+    pipeline()
