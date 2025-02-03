6776e8a986a68d2353b337fab476fe2ea87f6b88
Kieran <canter.kieran@gmail.com>
2025-02-02 23:28:47 -0500

    Added spinner and progress bar (indeterminate)

--- data/etl/main.py
+++ data/etl/main.py
@@ -3,8 +3,11 @@
 ###
 
 import sys
+import time
 import yaml
 from pathlib import Path
+import json
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn, TextColumn
 
 # Extract imports
 from extract import fetch_replay_ids, fetch_replays_by_id
@@ -19,7 +22,6 @@ from load import load_to_postgres, load_to_s3
 def load_config():
     with open(Path(__file__).parent.parent.parent / "config.yaml", "r") as file:
         return yaml.safe_load(file)
-    
 
 def pipeline(days_back: int, upload_to_postgres: bool, upload_to_s3: bool) -> None:
     ###
@@ -40,36 +42,43 @@ def pipeline(days_back: int, upload_to_postgres: bool, upload_to_s3: bool) -> No
             ranks_list = ranks["unranked"] if playlist.startswith("unranked") else ranks["ranked"]
 
             for rank in ranks_list:
-                replay_ids = fetch_replay_ids(replay_date, playlist, rank)
-                total_replays += len(replay_ids)
+                fetch_string = f"Fetching replay IDs for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}"
 
-                print(f"Fetched {len(replay_ids)} replays for {playlist} - {rank} on {replay_date.strftime('%Y-%m-%d')}")
+                # Fetch the replay IDs with progress spinner
+                with Progress(
+                    TimeElapsedColumn(),
+                    SpinnerColumn(),
+                    TextColumn("[progress.description]{task.description}"),
+                ) as progress:
+                    progress.add_task(description=fetch_string, total=None)
+                    replay_ids = fetch_replay_ids(replay_date, playlist, rank)
                 
-                # Get in-depth replay data for each replay ID and add to daily_replays
+                total_replays += len(replay_ids)
+                print(f"Fetched {len(replay_ids)} replay IDs for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}")
+                
+                # Get in-depth replay data for each replay ID
                 if replay_ids:
-                    filtered_replays = fetch_replays_by_id(replay_ids)
-                    print(filtered_replays)
+                    fetch_string = f"Fetching {len(replay_ids)} replays for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}"
+
+                    # Fetch replays with progress spinner
+                    with Progress(
+                        TimeElapsedColumn(),
+                        SpinnerColumn(),
+                        TextColumn("[progress.description]{task.description}"),
+                    ) as progress:
+                        progress.add_task(description=fetch_string, total=None)
+                        filtered_replays = fetch_replays_by_id(replay_ids)
 
                     if filtered_replays:
-                        if upload_to_postgres:
+                        print(f"Fetched {len(filtered_replays)} replays for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}\n")
+                        if upload_to_postgres or upload_to_s3:
                             daily_replays.extend(filtered_replays)
 
-                        if upload_to_s3:
-                            ###
-                            # Step 2: Transform for S3
-                            ###
-                            compressed_replays = compress_replays_for_s3(filtered_replays)
-
-                            ###
-                            # Step 3: Load Data into S3
-                            ###
-                            load_to_s3(compressed_replays, replay_date, playlist, rank)
-
-        if upload_to_postgres:
+        if upload_to_postgres or upload_to_s3:
             ###
             # Step 2: Transform for Postgres
             ###
-            # TODO: any proper transformations needed to load into postgres
+            # TODO: Transform data to prepare for bulk insert
 
             ###
             # Step 3: Load Data into Postgres
@@ -77,6 +86,9 @@ def pipeline(days_back: int, upload_to_postgres: bool, upload_to_s3: bool) -> No
             load_to_postgres(daily_replays)
 
         print(f"Finished fetching {total_replays} replays for {'today' if day == 0 else {'day back' if day == 1 else 'days back'}} ({replay_date.strftime('%Y-%m-%d')})\n")
+    
+    with open("test.json", "w") as file:
+        json.dump(daily_replays, file)
 
     print(f"Finished fetching {total_replays} replays for the past {days_back} {'days' if days_back > 1 else 'day'}")
 
