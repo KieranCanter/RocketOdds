d9bc18eacd360f06724ea5f1d2e38a95b3f6b022
Kieran <canter.kieran@gmail.com>
2025-05-01 07:09:29 -0400

    Added rate limit flag option and validation

--- data/etl/scripts/main.py
+++ data/etl/scripts/main.py
@@ -27,11 +27,61 @@ from .load import load_to_s3
 
 console = Console()
 
-def run_pipeline(lookback: int, upload_to_s3: bool, verbose: bool) -> None:
+def validate_rate_limit(rate_limit: str) -> str:
+    if not rate_limit:
+        console.log("No rate limit specified, using base rate limit.")
+        return "base"
+    
+    rate_limit = rate_limit.lower()
+    match rate_limit:
+        case "gc" | "grand champ" | "grand champion":
+            return "gc"
+        case "c" | "champ" | "champion":
+            return "c"
+        case "d" | "diamond":
+            return "d"
+        case "g" | "gold":
+            return "g"
+        case "base":
+            return "base"
+        case _:
+            raise ValueError(f"Invalid rate limit entered: {rate_limit}. Please choose one of the following or leave blank for the base rate limit: gc/grand champ/grand champion, c/champ/champion, d/diamond, g/gold, base")
+
+def get_replay_call_limits(rate_limit: str) -> tuple[int, int]:
+    match rate_limit:
+        case "gc" | "grand champ" | "grand champion":
+            return (16, None)
+        case "c" | "champ" | "champion":
+            return (8, None)
+        case "d" | "diamond":
+            return (4, 2000)
+        case "g" | "gold":
+            return (2, 1000)
+        case _:
+            return (2, 500)
+
+def get_id_call_limits(rate_limit: str) -> tuple[int, int]:
+    match rate_limit:
+        case "gc" | "grand champ" | "grand champion":
+            return (16, None)
+        case "c" | "champ" | "champion":
+            return (8, None)
+        case "d" | "diamond":
+            return (4, 5000)
+        case "g" | "gold":
+            return (2, 2000)
+        case _:
+            return (2, 1000)
+
+def run_pipeline(lookback: int, rate_limit: str, upload_to_s3: bool, verbose: bool) -> None:
+    rate_limit = validate_rate_limit(rate_limit)
+    replays_per_second, replays_per_hour = get_replay_call_limits(rate_limit)
+    ids_per_second, ids_per_hour = get_id_call_limits(rate_limit)
+    
     ###
     # Step 1: Extract Data from Ballchasing API
     ###
-    console.log(f"Running ETL pipeline for {lookback} {'day' if lookback == 1 else 'days'}{' and uploading to S3' if upload_to_s3 else ''}.")
+    console.log(f"Running ETL pipeline for {lookback} {'day' if lookback == 1 else 'days'} at the {rate_limit.title()} rate limit {' and uploading to S3' if upload_to_s3 else ''}.")
     if verbose:
         console.log(locals())
     
@@ -58,7 +108,7 @@ def run_pipeline(lookback: int, upload_to_s3: bool, verbose: bool) -> None:
                     TextColumn("[progress.description]{task.description}"),
                 ) as progress:
                     progress.add_task(description=fetch_string, total=None)
-                    replay_ids = fetch_replay_ids(replay_date, playlist, rank, calls_per_second=2, calls_per_hour=500)
+                    replay_ids = fetch_replay_ids(replay_date, playlist, rank, calls_per_second=replays_per_second, calls_per_hour=replays_per_hour)
                 
                 total_replays += len(replay_ids)
                 console.log(f"Fetched {len(replay_ids)} replay IDs for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}")
@@ -74,7 +124,7 @@ def run_pipeline(lookback: int, upload_to_s3: bool, verbose: bool) -> None:
                         TextColumn("[progress.description]{task.description}"),
                     ) as progress:
                         progress.add_task(description=fetch_string, total=None)
-                        filtered_replays = fetch_replays_by_id(replay_ids, calls_per_second=2, calls_per_hour=500)
+                        filtered_replays = fetch_replays_by_id(replay_ids, calls_per_second=ids_per_second, calls_per_hour=ids_per_hour)
 
                     if filtered_replays:
                         console.log(f"Fetched {len(filtered_replays)} replays for {rank} in {playlist} on {replay_date.strftime('%Y-%m-%d')}\n")
@@ -115,11 +165,12 @@ def main():
     run_parser = subparsers.add_parser("run", help="Run the ETL pipeline")
     run_parser.add_argument("-l", "--lookback", type=int, default=90, 
                             help="Number of days back to fetch replays for (default: 90)")
+    run_parser.add_argument("-r", "--rate-limit", type=str, nargs="?", default="base",
+                            help="Rate limit for Ballchasing API calls (default: base)")
     run_parser.add_argument("-s3", "--upload-to-s3", action="store_true", 
                             help="Upload to S3")
     run_parser.add_argument("-v", "--verbose", action="store_true", 
                             help="Enable verbose logging")
-
     # clean subcommand
     clean_parser = subparsers.add_parser("clean", help="Clean artifacts")
     clean_parser.add_argument("-v", "--verbose", action="store_true", 
@@ -138,7 +189,7 @@ def main():
     args = parser.parse_args()
     match args.subcommand:
         case "run":
-            run_pipeline(args.lookback, args.upload_to_s3, args.verbose)
+            run_pipeline(args.lookback, args.rate_limit, args.upload_to_s3, args.verbose)
         case "clean":
             clean_pipeline(args.verbose)
         case "status":
