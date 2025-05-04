7d2c83240b946e4fe9ce68036aa3879662cce01e
Kieran <canter.kieran@gmail.com>
2025-05-04 19:32:37 -0400

    Refactored config as a class

--- data/etl/scripts/config.py
+++ data/etl/scripts/config.py
@@ -1,23 +1,43 @@
 from pathlib import Path
 import yaml
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-def load_config():
-    config_path = Path(__file__).parent.parent.parent.parent
-    config_file = config_path / "config.yaml"
+class Config:
+    def __init__(self) -> None:
+        self.yaml = self._load_yaml()
+        self.env = self._load_env()
 
-    if not config_file.exists():
-        raise FileNotFoundError(
-            f"Could not find config.yaml in {config_path}"
-        )
+    def _load_yaml(self) -> dict:
+        config_path = Path(__file__).parent.parent.parent.parent
+        config_file = config_path / "config.yaml"
 
-    with open(config_file, "r") as file:
-        return yaml.safe_load(file) 
+        if not config_file.exists():
+            raise FileNotFoundError(
+                f"Could not find config.yaml in {config_path}"
+            )
 
-def load_env():
-    env_path = Path(__file__).parent.parent.parent.parent / ".env"
-    if not env_path.exists():
-        raise FileNotFoundError(
-            f"Could not find .env in {env_path}"
-        )
-    load_dotenv(dotenv_path=env_path)
+        with open(config_file, "r") as file:
+            return yaml.safe_load(file) 
+
+    def _load_env(self) -> dict:
+        env_path = Path(__file__).parent.parent.parent.parent / ".env"
+        if not env_path.exists():
+            raise FileNotFoundError(
+                f"Could not find .env in {env_path}"
+            )
+        return dotenv_values(dotenv_path=env_path)
+
+    def get_yaml(self) -> dict:
+        return self.yaml
+    
+    def get_ballchasing_api_key(self) -> str:
+        return self.env["BALLCHASING_API_KEY"]
+    
+    def get_aws_credentials(self) -> dict:
+        return {
+            "aws_data_access_key": self.env["AWS_DATA_ACCESS_KEY"],
+            "aws_data_secret_key": self.env["AWS_DATA_SECRET_KEY"],
+            "aws_region": self.env["AWS_REGION"],
+            "aws_data_bucket_name": self.env["AWS_DATA_BUCKET_NAME"],
+            "aws_models_bucket_name": self.env["AWS_MODELS_BUCKET_NAME"]
+        }
