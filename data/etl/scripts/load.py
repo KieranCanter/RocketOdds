5cbcfb01386290b8f86987c66ae8b1b9866264f6
Kieran <canter.kieran@gmail.com>
2025-05-08 00:08:00 -0400

    Changed filepath to key_path

--- data/etl/scripts/load.py
+++ data/etl/scripts/load.py
@@ -16,21 +16,19 @@ class ReplayLoader:
         )
         self.log = log
 
-    def _load(self, data_buffer, filepath):
+    def _load(self, data_buffer, key_path):
         try:
-            response = self.s3_client.put_object(Bucket="rocketodds-data", Key=filepath, Body=data_buffer, StorageClass="STANDARD")
+            response = self.s3_client.put_object(Bucket="rocketodds-data", Key=key_path, Body=data_buffer, StorageClass="STANDARD")
         except Exception as e:
-            self.log.error(f"Failed to upload {filepath} to S3. Status Code: "\
+            self.log.error(f"Failed to upload {key_path} to S3. Status Code: "\
                             f"{response['ResponseMetadata']['HTTPStatusCode']}\n")
             raise e
-
-        self.log.info(f"[green]Successfully uploaded {filepath} to S3 with ETag: {response['ETag']}[/green]\n")
 
     def run(self, transformed_replays, date, playlist, rank):
         data_table = pa.Table.from_pylist(transformed_replays)
         data_buffer = io.BytesIO()
         pq.write_table(data_table, data_buffer)
         data_buffer.seek(0)
-        filepath = f"{date.strftime('%Y')}/{date.strftime('%m')}/{date.strftime('%d')}"\
+        key_path = f"{date.strftime('%Y')}/{date.strftime('%m')}/{date.strftime('%d')}"\
                     f"/{playlist}/{date.strftime('%Y-%m-%d')}_{playlist}_{rank}.parquet"
-        self._load(data_buffer, filepath)
+        self._load(data_buffer, key_path)
