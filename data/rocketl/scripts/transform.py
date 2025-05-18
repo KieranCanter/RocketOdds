23e47222946c08239dc3e1983d7dad6d4df6a7c0
Kieran <canter.kieran@gmail.com>
2025-05-18 18:23:03 -0400

    Explicitly assigned transformd replay data instead of removing from existing response data

--- data/rocketl/scripts/transform.py
+++ data/rocketl/scripts/transform.py
@@ -21,34 +21,53 @@ class ReplayTransformer:
         return date
 
     def _transform_replay(self, replay: dict) -> dict:
-        # Pop unneeded fields, rename and flatten some fields, and convert dates to UTC datetimes
-        replay["upload_date"] = self._convert_to_utc_datetime(replay.pop("created", None))
-        for key in replay.get("uploader", {}).keys():
-            if key == "steam_id" or key == "name":
-                replay[key] = replay["uploader"].get(key, None)
-        replay.pop("uploader", None)
-        replay.pop("status", None)
-        replay.pop("match_type", None)
-        replay.pop("season_type", None)
-        replay["match_date"] = self._convert_to_utc_datetime(replay.pop("date", None))
-        replay.pop("date", None)
-        replay.pop("date_has_timezone", None)
-        replay.get("min_rank", {}).pop("name", None)
-        replay.get("max_rank", {}).pop("name", None)
-        replay.pop("playlist_name", None)
-        replay.pop("map_name", None)
-        replay.pop("server", None)
+        res = {}
+        res["id"] = replay.get("id", None)
+        res["link"] = replay.get("link", None)
+        res["created"] = replay.get("created", None)
+        res["steam_id"] = replay.get("uploader", {}).get("steam_id", None)
+        res["name"] = replay.get("uploader", {}).get("name", None)
+        res["rocket_league_id"] = replay.get("rocket_league_id", None)
+        res["match_guid"] = replay.get("match_guid", None)
+        res["title"] = replay.get("title", None)
+        res["map_code"] = replay.get("map_code", None)
+        res["map_name"] = replay.get("map_name", None)
+        res["team_size"] = replay.get("team_size", None)
+        res["playlist_id"] = replay.get("playlist_id", None)
+        res["duration"] = replay.get("duration", None)
+        res["overtime"] = replay.get("overtime", None)
+        res["overtime_seconds"] = replay.get("overtime_seconds", None)
+        res["season"] = replay.get("season", None)
+        res["date"] = replay.get("date", None)
+        res["date_has_timezone"] = replay.get("date_has_timezone", None)
+        res["visibility"] = replay.get("visibility", None)
+        res["min_rank"] = replay.get("min_rank", None)
+        res["max_rank"] = replay.get("max_rank", None)
         for team in ["blue", "orange"]:
-            replay[team].pop("color", None)
+            res[team] = {}
+            res[team]["players"] = []
             for player in replay.get(team, {}).get("players", []):
-                player["name"] = player.get("name", None)
-                player["platform"] = player.get("id", {}).pop("platform", None)
-                player["id"] = player.get("id", {}).pop("id", None)
-                player.pop("id", None)
-                player.pop("mvp", None)
-                player.get("rank", {}).pop("name", None)
+                res[team]["players"].append({
+                    "start_time": player.get("start_time", None),
+                    "end_time": player.get("end_time", None),
+                    "name": player.get("name", None),
+                    "platform": player.get("id", {}).get("platform", None),
+                    "id": player.get("id", {}).get("id", None),
+                    "rank": {
+                        "id": player.get("rank", {}).get("id", None),
+                        "tier": player.get("rank", {}).get("tier", None),
+                        "division": player.get("rank", {}).get("division", None),
+                        "name": player.get("rank", {}).get("name", None)
+                    },
+                    "car_id": player.get("car_id", None),
+                    "car_name": player.get("car_name", None),
+                    "camera": player.get("camera", None),
+                    "steering_sensitivity": player.get("steering_sensitivity", None),
+                    "stats": player.get("stats", None)
+                })
+            res[team]["stats"] = replay.get(team, {}).get("stats", None)
 
-        return replay
+        return res
 
     def run(self, replay: dict) -> dict:
         return self._transform_replay(replay)
